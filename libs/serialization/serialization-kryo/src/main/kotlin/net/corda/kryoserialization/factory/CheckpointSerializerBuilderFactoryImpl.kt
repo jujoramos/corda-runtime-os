cc4e24528aed0a4755b7f164dee2ddcdfbc6b191
Alexey Kadyrov <alexey.kadyrov@r3.com>
2021-10-13 11:36:58 +0100

    ...

--- libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/factory/CheckpointSerializerBuilderFactoryImpl.kt
+++ libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/factory/CheckpointSerializerBuilderFactoryImpl.kt
@@ -5,9 +5,12 @@ import net.corda.kryoserialization.impl.KryoCheckpointSerializerBuilderImpl
 import net.corda.sandbox.SandboxGroup
 import net.corda.serialization.CheckpointSerializerBuilder
 import net.corda.serialization.factory.CheckpointSerializerBuilderFactory
+import net.corda.v5.crypto.DigestAlgorithmName
+import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
+import java.security.Security
 
 @Component(immediate = true, service = [CheckpointSerializerBuilderFactory::class])
 class CheckpointSerializerBuilderFactoryImpl @Activate constructor(
@@ -17,6 +20,9 @@ class CheckpointSerializerBuilderFactoryImpl @Activate constructor(
     override fun createCheckpointSerializerBuilder(
         sandboxGroup: SandboxGroup
     ): CheckpointSerializerBuilder {
+        //Security.addProvider(BouncyCastleProvider())
+        val digest = cryptoLibraryFactory.getDigestService()
+        digest.hash("hello".toByteArray(), DigestAlgorithmName.SHA2_256)
         return KryoCheckpointSerializerBuilderImpl(
             cryptoLibraryFactory.getKeyEncodingService(),
             sandboxGroup
